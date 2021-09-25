abf48f016963ea3ba9d1e672af9b4eb27ba1a866
ws2h02 <cky035@gmail.com>
2021-09-26 04:51:26 +0800

    Create RaceResource.java

---
+++ src/main/java/codeit/template/resource/RaceResource.java
@@ -0,0 +1,23 @@
+package codeit.template.resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class RaceResource {
+    Logger logger = LoggerFactory.getLogger(RaceResource.class);
+
+   @RequestMapping(value = "fixedrace",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String calculate(@RequestBody String body){
+       System.out.println(body);
+       String ans = "Joseph Jarosz,Caitlin Cully,Nelson Noss,Cortez Carranco,Shona Stanek,Rudolf Ravelo,Alysia Alejandro,Justin Jack,Britt Bisceglia,Amos Alward";
+       return ans;
+    }
+}
