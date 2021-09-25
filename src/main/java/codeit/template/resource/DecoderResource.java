613ccd81f8badfd6c565d47587d05610ab5dea29
ws2h02 <cky035@gmail.com>
2021-09-26 04:23:34 +0800

    Update DecoderResource.java

--- src/main/java/codeit/template/resource/DecoderResource.java
+++ src/main/java/codeit/template/resource/DecoderResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Random;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -20,6 +20,7 @@ public class DecoderResource {
     public LinkedHashMap<String, Object> calculate(@RequestBody String body){
 	   System.out.println(body);
 	   String temp;
+	   Random rand = new Random();
        JSONObject obj = new JSONObject(body);
 	   String[] anslist = new String[obj.getInt("num_slots")];
 	   for(int j = 0; j < anslist.length; j++) {
@@ -34,6 +35,9 @@ public class DecoderResource {
         	   temp = i.substring(i.indexOf("output_received")+ 18, i.indexOf("]"));
         	   temp = temp.replace("\"", "");
     		   temp = temp.replace(",", "");
+    		   for(int j = 0; j < anslist.length; j++) {
+    			   anslist[j] = obj.getJSONArray("possible_values").get(rand.nextInt(anslist.length)).toString();
+    		   }
         	   System.out.println(result);
         	   if(result == 4) {
         		   for(int j = 0; j < anslist.length; j++) {
