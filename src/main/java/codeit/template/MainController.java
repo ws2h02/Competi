3d41a0e49ec05056f30f5cde8cc0e1301a6adb11
ws2h02 <cky035@gmail.com>
2021-09-25 10:27:11 +0800

    Update MainController.java

--- src/main/java/codeit/template/MainController.java
+++ src/main/java/codeit/template/MainController.java
@@ -1,5 +1,7 @@
 package codeit.template;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainController {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainController.class,args);
+    	SpringApplication app = new SpringApplication(MainController.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "5000"));
+    	app.run(args);
     }
 }
