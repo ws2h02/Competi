c948b162ffe3ec268f96cb3929ddda912d80f47e
ws2h02 <cky035@gmail.com>
2021-09-25 16:34:26 +0800

    Create StonksResource.java

---
+++ src/main/java/codeit/template/resource/StonksResource.java
@@ -0,0 +1,23 @@
+package codeit.template.resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class StonksResource {
+    Logger logger = LoggerFactory.getLogger(StonksResource.class);
+
+   @RequestMapping(value = "stonks",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Integer calculateSquare(@RequestBody Map<String, String> body){
+       Integer value = Integer.parseInt(body.get("input"));
+       logger.info("My Result--> {}",value);
+       return value* value;
+    }
+}
