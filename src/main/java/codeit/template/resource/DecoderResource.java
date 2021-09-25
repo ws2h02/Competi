fdbd4ea6b5669f95f74097a96a2f585257df0a28
ws2h02 <cky035@gmail.com>
2021-09-26 03:11:13 +0800

    Create DecoderResource.java

---
+++ src/main/java/codeit/template/resource/DecoderResource.java
@@ -0,0 +1,28 @@
+package codeit.template.resource;
+
+import org.json.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class DecoderResource {
+    Logger logger = LoggerFactory.getLogger(DecoderResource.class);
+
+   @RequestMapping(value = "decoder",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public LinkedHashMap<String, Object> calculate(@RequestBody String body){
+	   
+       JSONObject obj = new JSONObject(body);
+       LinkedHashMap<String, Object> ans = new LinkedHashMap();
+       String[] anslist = {"a", "b", "d", "c"};
+       ans.put("answer", anslist);
+	return ans;
+    }
+}
