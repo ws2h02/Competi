e7c8355d426c86a35b42174ac88ab750d5991249
ws2h02 <cky035@gmail.com>
2021-09-26 08:33:47 +0800

    Update StonksResource.java

--- src/main/java/codeit/template/resource/StonksResource.java
+++ src/main/java/codeit/template/resource/StonksResource.java
@@ -1,5 +1,7 @@
 package codeit.template.resource;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
@@ -15,9 +18,16 @@ public class StonksResource {
     Logger logger = LoggerFactory.getLogger(StonksResource.class);
 
    @RequestMapping(value = "stonks",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Integer calculateSquare(@RequestBody Map<String, String> body){
-       Integer value = Integer.parseInt(body.get("input"));
-       logger.info("My Result--> {}",value);
-       return value* value;
+    public LinkedHashMap<String, Object>[] calculate(@RequestBody String body){
+	   System.out.println(body);
+	   JSONArray obj = new JSONArray(body);
+	   for(Object i: obj) {
+		   JSONObject Here = new JSONObject(i.toString());
+		   JSONObject tmp = new JSONObject(Here.get("timeline").toString());
+		   
+	   }
+	   
+	   LinkedHashMap<String, Object>[] ans = new LinkedHashMap[obj.length()];
+	   return ans;
     }
 }
