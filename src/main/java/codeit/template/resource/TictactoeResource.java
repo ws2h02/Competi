8a64611b0a9fa65ed9c91148facb6eaeefd1c07f
ws2h02 <cky035@gmail.com>
2021-09-26 09:23:52 +0800

    Create TictactoeResource.java

---
+++ src/main/java/codeit/template/resource/TictactoeResource.java
@@ -0,0 +1,23 @@
+package codeit.template.resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TictactoeResource {
+    Logger logger = LoggerFactory.getLogger(TictactoeResource.class);
+
+   @RequestMapping(value = "tic-tac-toe",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Integer calculateSquare(@RequestBody Map<String, String> body){
+       Integer value = Integer.parseInt(body.get("input"));
+       logger.info("My Result--> {}",value);
+       return value* value;
+    }
+}
